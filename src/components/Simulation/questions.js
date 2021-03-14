62d051e6683c588f33356c0dc1f6d3948b1a58b6
Justus <justus.soh@hotmail.com>
2021-03-14 23:54:05 +0800

    feat: added siren

--- src/components/Simulation/questions.js
+++ src/components/Simulation/questions.js
@@ -1,15 +1,30 @@
 export const questions = [
   {
-    question: "test",
+    question: "test1",
     options: [
       {
         answer: "answer 1",
-        value: 0,
+        value: 100,
       },
       {
-        answer: "answer 1",
-        value: 0,
+        answer: "answer 2",
+        value: -50,
       },
     ],
+    skip: 10,
+  },
+  {
+    question: "test2",
+    options: [
+      {
+        answer: "answer 1",
+        value: 100,
+      },
+      {
+        answer: "answer 2",
+        value: -50,
+      },
+    ],
+    skip: 10,
   },
 ];
--- src/components/Simulation/simulation.js
+++ src/components/Simulation/simulation.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import useSound from "use-sound";
 import { useStickyState } from "../../common/hooks/useStickyState";
 import { IconButton } from "@material-ui/core";
@@ -7,8 +7,12 @@ import VolumeUpRoundedIcon from "@material-ui/icons/VolumeUpRounded";
 import TimerOffRoundedIcon from "@material-ui/icons/TimerOffRounded";
 import TimerRoundedIcon from "@material-ui/icons/TimerRounded";
 import RadioButtonCheckedRoundedIcon from "@material-ui/icons/RadioButtonCheckedRounded";
+import RadioButtonUncheckedRoundedIcon from "@material-ui/icons/RadioButtonUncheckedRounded";
+import FlareRoundedIcon from "@material-ui/icons/FlareRounded";
 
 import { WelcomeModal } from "./welcomeModal";
+
+import { questions } from "./questions";
 
 import "./simulation.css";
 
@@ -16,12 +20,12 @@ import bgmSfx from "../../common/audio/Tracker.mp3";
 import countdownSfx from "../../common/audio/beep.wav";
 
 const TIMER_LIMIT = 5;
-const numQuestions = 10;
 
 export const Simulation = () => {
   // Global Control
   const [soundEnabled, setSoundEnabled] = useStickyState(true, "soundEnabled");
   const [timerEnabled, setTimerEnabled] = useStickyState(true, "timerEnabled");
+  const [sirenEnabled, setSirenEnabled] = useStickyState(true, "sirenEnabled");
 
   // Sound Control
   const [playBgm, { sound: bgm }] = useSound(bgmSfx, {
@@ -47,45 +51,114 @@ export const Simulation = () => {
   };
 
   // Timer Control
-  const [timer, setTimer] = useState(0);
-  useEffect(() => {
-    if (timerEnabled) {
-      setTimeout(() => {
-        if (timer > 0) {
-          setTimer(timer - 1);
-          playCountdown();
-        }
-      }, 1000);
-    }
-  });
+  const [timeLeft, setTimeLeft] = useState(0);
 
   const onToggleTimer = () => {
     setTimerEnabled(!timerEnabled);
   };
 
+  // Siren Control
+  const onToggleSiren = () => {
+    setSirenEnabled(!sirenEnabled);
+  };
+
+  // Game State
+  const [gameState, setGameState] = useState("START");
+  const [gameIndex, setGameIndex] = useState(0);
+
+  const nextQuestion = useCallback(() => {
+    if (gameIndex === questions.length - 1) {
+      console.log("finish");
+
+      setGameState("END");
+      setGameIndex(0);
+    } else {
+      console.log("next");
+      setGameIndex(gameIndex + 1);
+      setTimeLeft(TIMER_LIMIT);
+    }
+  }, [gameIndex]);
+
+  useEffect(() => {
+    // Don't count if no timer
+    if (!timerEnabled || gameState !== "GAME") return;
+
+    if (timeLeft === -1) return nextQuestion();
+
+    // exit early when we reach 0
+    // if (!timeLeft) return;
+    // save intervalId to clear the interval when the
+    // component re-renders
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+      playCountdown();
+      console.log(timeLeft);
+      // console.log("index: " + gameIndex);
+    }, 1000);
+
+    // clear interval on re-render to avoid memory leaks
+    return () => clearInterval(intervalId);
+    // add timeLeft as a dependency to re-rerun the effect
+    // when we update it
+  }, [timeLeft, nextQuestion, timerEnabled, gameState, playCountdown]);
+
+  const renderGame = () => {
+    switch (gameState) {
+      case "START":
+        return (
+          <div>
+            Simulation
+            <button
+              onClick={() => {
+                setGameState("GAME");
+                setTimeLeft(TIMER_LIMIT);
+              }}
+            >
+              START
+            </button>
+          </div>
+        );
+      case "GAME":
+        return (
+          <div>
+            <RadioButtonCheckedRoundedIcon className="blink red" />
+            {timerEnabled ? <code>{timeLeft}</code> : ""}
+            <h1>{questions[gameIndex].question}</h1>
+            <button onClick={nextQuestion}>
+              {questions[gameIndex].options[0].answer}
+            </button>
+            <button onClick={nextQuestion}>
+              {questions[gameIndex].options[1].answer}
+            </button>
+          </div>
+        );
+      case "END":
+        return <button onClick={() => setGameState("START")}>restart</button>;
+      default:
+        return <div />;
+    }
+  };
+
   return (
     <header className="App-header">
-      <WelcomeModal />
-      Simulation
-      <div>
-        <code>{timer}</code>
-        <button
-          onClick={() => {
-            setTimer(TIMER_LIMIT);
-          }}
-        >
-          Boop
-        </button>
-      </div>
-      <RadioButtonCheckedRoundedIcon className="blink red" />
-      <div>
+      {sirenEnabled ? <div className="siren level-1" /> : ""}
+      <div className="level-2">{renderGame()}</div>
+      <div className="level-2">
         <IconButton onClick={onMute}>
           {!soundEnabled ? <VolumeOffRoundedIcon /> : <VolumeUpRoundedIcon />}
         </IconButton>
         <IconButton onClick={onToggleTimer}>
           {!timerEnabled ? <TimerOffRoundedIcon /> : <TimerRoundedIcon />}
         </IconButton>
+        <IconButton onClick={onToggleSiren}>
+          {!sirenEnabled ? (
+            <RadioButtonUncheckedRoundedIcon />
+          ) : (
+            <FlareRoundedIcon />
+          )}
+        </IconButton>
       </div>
+      <WelcomeModal />
     </header>
   );
 };
