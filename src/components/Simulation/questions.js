1ee1735c96a12d8fe1da368679cf878df2c5a980
Justus <justus.soh@hotmail.com>
2021-03-25 17:18:15 +0800

    feat: added question 20-40

--- src/components/Simulation/questions.js
+++ src/components/Simulation/questions.js
@@ -408,6 +408,387 @@ export const questions = [
     skip: 50,
   },
   {
+    question: "You need to get a house, but the wait time is 7 years, do you:",
+    options: [
+      {
+        answer: "Pay more to get the house now",
+        value: 0,
+        wealth: -100,
+      },
+      {
+        answer: "Rent a place",
+        value: -10,
+        wealth: -20,
+      },
+      {
+        answer: "My social credit let me jumps the queue for public housing",
+        value: 10,
+        wealth: -50,
+        range: {
+          minScore: 800,
+          maxScore: 1500,
+        },
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question:
+      "You need to make a big down payment for a house, but you don’t have enough cash, do you:",
+    options: [
+      {
+        answer: "Make a late payment, it is difficult to get loans",
+        value: -30,
+        wealth: -50,
+      },
+      {
+        answer: "Loan from a relative",
+        value: -10,
+        wealth: -20,
+        range: {
+          minScore: 400,
+          maxScore: 1500,
+        },
+      },
+      {
+        answer: "Wait, my credit score allows me to loans from the bank easily",
+        value: 0,
+        range: {
+          minScore: 800,
+          maxScore: 1500,
+        },
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "Would you accept military service if it was requested of you?",
+    options: [
+      {
+        answer: "Yes",
+        value: 50,
+      },
+      {
+        answer: "No",
+        value: -30,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "Have you ever donated your blood to the blood bank?",
+    options: [
+      {
+        answer: "Yes",
+        value: 50,
+      },
+      {
+        answer: "No",
+        value: -30,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "You are new to the neighborhood, do you:",
+    options: [
+      {
+        answer: "Introduce yourself to the neighbors",
+        value: 10,
+      },
+      {
+        answer: "Move in quietly",
+        value: 0,
+      },
+      {
+        answer: "Get your neighbor moving in gifts",
+        value: 20,
+        wealth: -30,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question:
+      "Your neighbor's potted plants obstruct the way along the pavement, do you:",
+    options: [
+      {
+        answer: "Ignore it and give your neighbors slack",
+        value: -20,
+      },
+      {
+        answer: "Report them to the relevant channel",
+        value: 10,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question:
+      "Your dog just pooped at the void deck and you forgot to being a bag, would you:",
+    options: [
+      {
+        answer: "Pick it up and dispose of it",
+        value: 10,
+      },
+      {
+        answer: "Come back later",
+        value: -10,
+      },
+      {
+        answer: "Ignore and hope no one finds out",
+        value: -20,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "You urgently need to use the restroom, would you:",
+    options: [
+      {
+        answer: "Jump the queue",
+        value: -20,
+      },
+      {
+        answer: "Drop a $50 note and run into the restroom amidst the chaos",
+        value: 0,
+        wealth: -50,
+      },
+    ],
+    skip: 50,
+  },
+
+  {
+    question:
+      "You are occupying a seat on the bus when an elderly person gets in:",
+    options: [
+      {
+        answer:
+          "Wait for someone to get up first and offer seat if no one does",
+        value: -10,
+      },
+      {
+        answer: "Get up immediately",
+        value: 10,
+      },
+      {
+        answer: "Pretend you haven’t noticed",
+        value: -20,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "You see a student on flag day do you:",
+    options: [
+      {
+        answer: "Avoid them at all cost",
+        value: -10,
+      },
+      {
+        answer: "Give smallest amount you have",
+        value: 10,
+        wealth: -20,
+      },
+      {
+        answer: "Give all you can afford",
+        value: 20,
+        wealth: -50,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "You make a new year resolution to be fitter this year, do you:",
+    options: [
+      {
+        answer:
+          "You have been denied access to the facility, try again next year",
+        value: -20,
+      },
+      {
+        answer: "Pay for a gym membership",
+        value: 10,
+        wealth: -50,
+        range: {
+          minScore: 400,
+          maxScore: 1500,
+        },
+      },
+      {
+        answer: "My credit score allows me to get free gym facilities",
+        value: 10,
+        range: {
+          minScore: 800,
+          maxScore: 1500,
+        },
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "Do you play games?",
+    options: [
+      {
+        answer: "Yes, whenever I am free",
+        value: 0,
+      },
+      {
+        answer: "Yes, all the time",
+        value: -10,
+      },
+      {
+        answer: "No, I don’t really have time for that",
+        value: 10,
+        goto: 34,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "On the average, how many games do you purchase a year?",
+    options: [
+      {
+        answer: "1 – 5",
+        value: 0,
+      },
+      {
+        answer: "6 – 10",
+        value: -10,
+      },
+      {
+        answer: "11 and above",
+        value: -20,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "Do you try to find loopholes within the rules?",
+    options: [
+      {
+        answer: "Yes, no harm trying",
+        value: -10,
+      },
+      {
+        answer: "Yes, there is always a way around it",
+        value: -20,
+      },
+      {
+        answer: "No, never thought of it",
+        value: 20,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "Do you consume digital media like TV, films, or Baidu?",
+    options: [
+      {
+        answer: "Yes, whenever I am free ",
+        value: 0,
+      },
+      {
+        answer: "Yes, all the time",
+        value: -10,
+      },
+      {
+        answer: "No, I don’t really have time for that",
+        value: 10,
+        goto: 37,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question:
+      "You enjoy a show that depict the government in a bad way, would continue watching it after finding out?",
+    options: [
+      {
+        answer: "Yes",
+        value: -20,
+      },
+      {
+        answer: "No",
+        value: 10,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "How often do you visit the cinemas?",
+    options: [
+      {
+        answer: "Never, I can find shows online for free",
+        value: -20,
+      },
+      {
+        answer: "Very often, I love the movies",
+        value: 0,
+      },
+      {
+        answer: "Occasional, when a good show is out",
+        value: 0,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question: "How often do you volunteer to help the needy?",
+    options: [
+      {
+        answer: "Bi-Weekly when I have the time",
+        value: 50,
+      },
+      {
+        answer: "When my work organizes an event",
+        value: 20,
+      },
+      {
+        answer: "That not my kind of thing, I don’t really have time for that",
+        value: -20,
+        goto: 40,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question:
+      "The orphanage you are volunteering is calling for donations, do you:",
+    options: [
+      {
+        answer: "Ignore and pretend that nothing happened",
+        value: -10,
+      },
+      {
+        answer: "Donate $50 to the cause",
+        value: 20,
+        wealth: -50,
+      },
+    ],
+    skip: 50,
+  },
+  {
+    question:
+      "While volunteering, a less privileged person asked to link with you on the social credit system, do you:",
+    options: [
+      {
+        answer: "Politely decline",
+        value: -20,
+      },
+      {
+        answer: "Distract then with a red packet ",
+        value: 0,
+        wealth: -50,
+      },
+      {
+        answer: "Accept the request ",
+        value: -10,
+      },
+    ],
+    skip: 50,
+  },
+  {
     question: "Do you believe in any religion?",
     options: [
       {
@@ -416,6 +797,10 @@ export const questions = [
       },
       {
         answer: "No",
+        value: 30,
+      },
+      {
+        answer: "maybe",
         value: 30,
       },
     ],
